3e503d6a133bcfab48f0c402fa4b20c9e812bd21
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2022-12-13 16:51:53 -0300

    feat: add Languages (Remember Antarctica doesn't have a language)

--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,6 +1,8 @@
 import styles from './styles.module.css'
 
 // (props) was unstructure to ({data}), so you can use just {data} instead of {props.data}
+
+// conditional ternary operator is used in Languages to avoid "undefined" from Antarctica
 export const Country = ({data}) => {
     return (
         <div className={styles.card}>
@@ -10,6 +12,7 @@ export const Country = ({data}) => {
                 <ul className={styles.info}>
                     <li>Capital: {data.capital}</li>
                     <li>Region: {data.region}</li>
+                    <li>Languages: {data.languages === undefined ? "None" : Object.values(data.languages).join(", ")}</li>
                 </ul>
             </div>
         </div>
